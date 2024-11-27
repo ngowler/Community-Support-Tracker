8b670d3d42af0dae99c6975622a6aa5051b9e935
Nick Gowler <ngowler@academic.rrc.ca>
2024-11-27 08:31:15 -0600

    Created functionality to volunteer form.

---
+++ script.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", load);
+
+function load() {
+    selectStar();
+    document.getElementById("volunteer-hours-form").addEventListener("submit", validateVolunteerForm);
+}
+
+function validateVolunteerForm(e) {
+    hideErrors()
+    if(formHasErrors()) {
+        e.preventDefault();
+    } else {
+        let volunteerData = {};
+        volunteerData.charityName = document.getElementById("charity-name").value;
+        volunteerData.hoursVolunteered = parseFloat(document.getElementById("hours-volunteered").value);
+        volunteerData.date = document.getElementById("volunteer-hours-date").value;
+        volunteerData.stars = document.getElementsByClassName("starsSelected").length;
+        localStorage.setItem("volunteerData", JSON.stringify(volunteerData));
+    }
+}
+
+function hideErrors() {
+    let errorFields = document.getElementsByClassName("volunteer-form-error");
+    for(let i=0; i<errorFields.length; i++) {
+        errorFields[i].style.display = "none";
+    }
+}
+
+function showError(formField, errorId, errorFlag) {
+	document.getElementById(errorId).style.display = "block";
+	if(!errorFlag) {
+		document.getElementById(formField).focus();
+		if(formField.type == "text") {
+			document.getElementById(formField).select();
+		}
+	}
+}
+
+function formHasErrors() {
+    let errorFlag = false
+    let charityName = document.getElementById("charity-name").value;
+    if(charityName == "" || charityName == null) {
+        showError("charity-name", "charity-name_error", errorFlag);
+        errorFlag=true;
+    }
+    let hoursVolunteered = document.getElementById("hours-volunteered").value;
+    if(hoursVolunteered < 0 || hoursVolunteered == "" || hoursVolunteered == null) {
+        showError("hours-volunteered", "hours-volunteered_error", errorFlag);
+        errorFlag=true;
+    }
+    let volunteerDate = document.getElementById("volunteer-hours-date").value;
+    if(volunteerDate == "" || volunteerDate == null) {
+        showError("volunteer-hours-date", "volunteer-hours-date_error", errorFlag);
+        errorFlag=true;
+    }
+    let numberOfStars = document.getElementsByClassName("starsSelected").length;
+    if(numberOfStars == 0){
+        showError("volunteer-experience-rating", "volunteer-experience-rating_error", errorFlag);
+        errorFlag=true;
+    }
+    return errorFlag;
+}
+
+function selectStar() {
+    const stars = Array.from(document.getElementsByClassName("star"));
+    stars.forEach((star) => {
+        star.addEventListener("click", () => {
+            resetStars();
+            let starValue = star.value;
+            for(i=0; i<starValue; i++) {
+                stars[i].classList.add("starsSelected");
+            }
+        });
+    });
+}
+
+function resetStars() {
+    const stars = Array.from(document.getElementsByClassName("star"));
+    stars.forEach((star) => {
+        star.classList.remove("starsSelected");
+    });
+}
