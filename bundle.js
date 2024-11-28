d474a629840224b330fc380cf4ff894a75b6896b
Tysen Heck <theck@academic.rrc.ca>
2024-11-28 14:05:23 -0600

    Added more to the style guide and added bundle.js to the tempalte file.

--- bundle.js
+++ bundle.js
@@ -1,36 +1,47 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+
 function load() {
   const donationSubmitButton = document.getElementById('donation-submit-button')
-  
+
+  const hamburgerMenu = document.getElementById("hamburgerMenuSVG");
+  let hamburgerMenuCount = 0;
+  const navbar = document.getElementById("navbar");
+  let mediaQuery = window.matchMedia("(max-width: 700px");
+
+  donationSubmitButton.addEventListener('click', (e) => donationValidateForm(e))
+  hamburgerMenu.addEventListener('click', handleMenuClick);
+  mediaQuery.addEventListener('change', () => handleMediaQuery(mediaQuery));
+  handleMediaQuery(mediaQuery)
+}
+
+// donationFormData = {}
+
+
+function donationValidateForm(e) {
+  e.preventDefault()
+  donationHideErrors();
+  errorFlag = false
+
   const donationInputs = [
     'donation-charity-name-',
     'donation-amount-',
     'donation-date-',
     'donation-message-'
   ]
-  
-  const donationFormData = {}
-  donationSubmitButton.addEventListener('click', (e) => validateForm(e))
-}
 
-
-function validateForm(e) {
-
-  hideErrors();
-  errorFlag = false
+  donationFormData = {}
 
   donationInputs.forEach((input) => {
-    if (!formHasInput(input)) {
+    if (!donationFormHasInput(input)) {
       errorFlag = true
     }
   })
 
   donationAmountInputValue = document.getElementById('donation-amount-input').value
 
-
   let numberRegexp = new RegExp(/^[0-9]+$/)
   if (!numberRegexp.test(donationAmountInputValue)) {
-    donationAmountErrorWrapper.style.display = 'flex';
+    document.getElementById('donation-amount-error-wrapper').style.display = 'flex';
     errorFlag = true
   }
 
@@ -40,21 +51,17 @@ function validateForm(e) {
     donationFormData['donationDate'] = document.getElementById('donation-date-input').value
     donationFormData['donationMessage'] = document.getElementById('donation-message-input').value
   }
-
-  if (errorFlag) {
-    e.preventDefault()
-  }
 }
 
 
-function hideErrors() {
+function donationHideErrors() {
   document.getElementById('donation-charity-name-error-wrapper').style.display = 'none';
   document.getElementById('donation-amount-error-wrapper').style.display = 'none';
   document.getElementById('donation-date-error-wrapper').style.display = 'none';
   document.getElementById('donation-message-error-wrapper').style.display = 'none';
 }
 
-function formHasInput(input) {
+function donationFormHasInput(input) {
   let inputElement = document.getElementById(input + 'input')
   
 
@@ -66,10 +73,33 @@ function formHasInput(input) {
   }
 }
 
+function handleMenuClick() {
+  if (hamburgerMenuCount == 0) {
+    hamburgerMenuCount++
+    navbar.style.left = '0px';
+  } else {
+    hamburgerMenuCount = 0
+    navbar.style.left = '-300px';
+  }
+}
+
+// Code used from this website https://www.w3schools.com/howto/howto_js_media_queries.asp
+function handleMediaQuery(mediaQuery) {
+  if (mediaQuery.matches) {
+    navbar.style.left = '-300px';
+    hamburgerMenuCount = 0
+  } else {
+    navbar.style.left = '0px';
+  }
+}
+
 if (typeof window !== "undefined") {
+
   window.onload = load;
 } else {
   // CommonJS-style exports are used when in a Node.js environment
-  module.exports = { validateForm, hideErrors, formHasInput };
+  module.exports = { donationValidateForm, donationHideErrors, donationFormHasInput };
 }
+
+
 },{}]},{},[1]);
